dd4982bedf46cb802443632a0c210f235bc22eff
Daniel Metcalf <daniel.metcalf@ymail.com>
2012-03-16 14:38:10 -0700

    First Upload

---
+++ index.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link href="./css/styles.css" rel="stylesheet" type="text/css" />
+
+<head>
+<div class="logo"></div>
+<ul id="nav">
+<a href="https://play.google.com/store"><image src="./images/play.png" alt="play" class="play" /><a/>
+<li id="active"><a href="#">Home</a></li>
+<li><a href="#">Information</a></li>
+<li><a href="#">Techniques</a></li>
+<li><a href="#">Support</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</head>
+<div id="end">
+<div id="logo">
+<a href="#"><p>Ice Cream Sandwich<p></a>
+<a href="http://www.xda-developers.com/"><div class="logo1"></div></a>
+</div>
+<div class="titles">
+<p1>
+</p1>
+<login>
+<form action="process.php" method="POST">
+<input type="text" name="user" maxlength="30" value="Username" id="input" class="user" />
+<input type="password" name="password" value="Password" id="input" class="pass" />
+<input type="checkbox" name="remember"><font size="2">Remember Me</font>
+<input type="hidden" name="sublogin" value="1">
+<input type="submit" class="user" value="" id="submit" />
+</login>
+</div>
+<div class="body">
+
+<!*****/////RIGHT\\\\\*****>
+<div id="boxright">
+<h3>
+Weekly Poll
+</h3>
+<p>Option 1<br />
+Option 2<br />
+Option 3<br />
+<br />
+<input type="submit" value="Submit" />
+</p>
+</div>
+
+
+<!*****/////LEFT\\\\\*****!>
+<div id="box">
+<h3>
+Ice Cream Sandwich Website
+</h3>
+<p>This is the first website ever to be based off of Google's own Ice Cream Sandwich or Android 4.0. It is still in development but it progressing along. Please check back later as I will be adding a login system, CMS, and other things to come.</p>
+</div>
+<div id="extra"><center><img src="./images/more.png" alt="hi" /><center></div>
+
+
+<div id="box">
+<h3>
+Created by SaviorXD
+</h3>
+<p>This site belongs to SaviorXD, please do not take from it or steal it alltogether.</p>
+</div>
+<div id="extra"></div>
+<div id="extra"><center><img src="./images/more.png" alt="hi" /><center></div>
+
+
+
+</div>
+</div>
+<footer>
+<br />
+<p1>©2012 by <a href="#">SaviorXD.</a>on ICS Theme. All rights reserved.<p1>
+</footer>
+</html>
